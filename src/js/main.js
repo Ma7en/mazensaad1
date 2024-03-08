38e6bdc648f4cf34e798e289a765bb8b4a2142d5
Ma7en <m9ee9m@gmail.com>
2024-03-08 19:48:39 +0200

    Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,21 @@
 
 /* =============== start header menu ================== */
 
-let navMenu = document.getElementById("nav-menu"),
+let navMenuS = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
     navClose = document.getElementById("nav-close");
 
 /* show menu */
 // if (navToggle) {
 navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+    navMenuS.classList.add("show-menu");
 });
 // }
 
 /* hidden menu */
 // if (navClose) {
 navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+    navMenuS.classList.remove("show-menu");
 });
 // }
 
